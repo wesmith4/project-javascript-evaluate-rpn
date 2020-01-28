9419449875302ffbcdfa2cafbb7a9252a4d64960
Will Smith <wismith@davidson.edu>
2020-01-28 11:59:36 -0800

    this program works

--- evaluateRPN.js
+++ evaluateRPN.js
@@ -13,6 +13,23 @@
  * @param {string} expr - An RPN expression
  * @returns {number} The number the input expression evaluates to
  */
+
+function condense(a,b,fn){
+  return fn(a,b);
+}
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a * b;
+}
+function divide(a,b){
+  return a / b;
+}
+const operationDict = {'+':add, '-':subtract, '*':multiply, '/':divide}; 
 function evaluateRPN(expr) {
   /*
     This is your job. :)
@@ -32,6 +49,41 @@ function evaluateRPN(expr) {
       - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
       - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
   */
+  let elements = expr.split(' ');
+
+  for (let i = 0; i < elements.length; i++){
+    if (Number(elements[i]) || Number(elements[i]) === 0){
+      elements[i] = Number(elements[i]);
+    }
+  }
+
+  if (elements.length === 1){
+    return elements[0];
+  }
+  let i = 0
+  while (elements.length >= 3){
+    if (typeof elements[i] === 'number' && typeof elements[i+1] === 'number' && ['+','-','*', '/'].includes(elements[i+2])){
+      elements.splice(i,3,condense(elements[i],elements[i+1], operationDict[elements[i+2]]));
+      i = 0;
+    } else {
+      i++;
+    }
+    
+  }
+  
+  
+  
+  /*
+  for (let i = 0; i < elements.length; i++){
+    if (Number(elements[i]) && Number(elements[i+1]) && ['+','-','*', '/'].includes(elements[i+2])){
+      elements.splice(i,3,condense(Number(elements[i]), Number(elements[i+1]), operationDict[elements[i+2]]));
+    }
+  }
+  */
+  
+  let result = elements[0];
+  return result;
+
 }
 
 if (require.main === module) {
@@ -39,6 +91,7 @@ if (require.main === module) {
 
   // Write more of your own sanity checks!
 
+  console.log(evaluateRPN('1 2 +') === 3);
   console.log(evaluateRPN('-12') === -12);
   console.log(evaluateRPN('0') === 0);
   console.log(evaluateRPN('12') === 12);
@@ -48,10 +101,11 @@ if (require.main === module) {
 
   console.log(evaluateRPN('5 4 -') === 5 - 4);
   console.log(evaluateRPN('4 5 -') === 4 - 5);
-
   console.log(evaluateRPN('2 10 4 - *') === 2 * (10 - 4));
-
   console.log(evaluateRPN('0 1 1 1 + - -') === 0 - (1 - (1 + 1)));
 }
+// 0 1 2 - - 
+// 0 -1 - 
+// 1
 
 module.exports = evaluateRPN;
